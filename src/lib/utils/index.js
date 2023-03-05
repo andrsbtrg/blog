e8724391986147a02c1bbdbd8a259f5685a3159a
Andres <andrsbtrg@gmail.com>
2023-03-05 22:05:08 +0100

    adding api to read posts

---
+++ src/lib/utils/index.js
@@ -0,0 +1,21 @@
+export const fetchMarkdownPosts = async () => {
+    const allPostFiles = import.meta.glob('/src/routes/blog/*.md')
+    const iterablePostFiles = Object.entries(allPostFiles)
+    
+    const allPosts = await Promise.all(
+      iterablePostFiles.map(async ([path, resolver]) => {
+        // @ts-ignore
+        const { metadata } = await resolver()
+        // since we know every post file path will begin with
+        // /src/routes and end with .md we can remove those chars
+        const postPath = path.slice(11, -3)
+  
+        return {
+          meta: metadata,
+          path: postPath,
+        }
+      })
+    )
+  
+    return allPosts
+  }
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+
+    import { each } from "svelte/internal";
+
+</script>
