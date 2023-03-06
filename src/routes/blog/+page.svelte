2e9564da39fb80b73df799851ff79d8bc37d2e37
Andres <andrsbtrg@gmail.com>
2023-03-06 15:20:54 +0100

    added api to fetch all posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,5 +1,20 @@
 <script>
-
-    import { each } from "svelte/internal";
-
+	/**
+	 * @type {{ posts: any; }}
+	 */
+	export let data;
 </script>
+
+<h1>Blog</h1>
+<ul>
+	{#each data.posts as post}
+		<li>
+			<h2>
+				<a href={post.path}>
+					{post.meta.title}
+				</a>
+			</h2>
+			Published {post.meta.date}
+		</li>
+	{/each}
+</ul>
